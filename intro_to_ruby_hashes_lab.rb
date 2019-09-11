13f7ade1a6c9a5b80dc3d3fc2cdfe03335fe57f6
James Schaffer <jas.schaffer@gmail.com>
2019-09-11 18:34:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,15 +3,18 @@ def new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  return ({name: "james"})
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  return ({name: "Grace Hopper"})
+
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  
 end
 
 def my_hash_creator(key, value)
